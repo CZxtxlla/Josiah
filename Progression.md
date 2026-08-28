a4c14bcb01689c4fc995ca0ee3b4462303793a15
Charles Zitella <cszitella@gmail.com>
2026-08-27 21:49:07 -0400

    added piece square tables

--- Progression.md
+++ Progression.md
@@ -2,3 +2,4 @@
 ---
 Version 0.0.1 is the first functioning version. It implements negamax alpha beta pruning with iterative deepening and basic time management, as well as a purely material based evaluation function.
 
+Version 0.0.2 adds piece square tables to the evaluation. Here is the results of sprt with version 0.0.1:
--- src/evaluate.c
+++ src/evaluate.c
@@ -1,16 +1,141 @@
 #include "evaluate.h"
 
+// piece square tables (copied from wiki)
+// must flip rank
+
+const int PAWN_MG_TABLE[64] = {
+    0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    5, 10, 10,-20,-20, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0
+};
+
+const int KNIGHT_TABLE[64] = {
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50
+};
+
+const int BISHOP_TABLE[64] = {
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20
+};
+
+const int ROOK_TABLE[64] = {
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    0,  0,  0,  5,  5,  0,  0,  0
+};
+
+const int QUEEN_TABLE[64] = {
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+    -5,  0,  5,  5,  5,  5,  0, -5,
+    0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+};
+
+const int KING_MG_TABLE[64] = {
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+    20, 20,  0,  0,  0,  0, 20, 20,
+    20, 30, 10,  0,  0, 10, 30, 20
+};
+
+const int PAWN_EG_TABLE[64] = {
+    0,  0,  0,  0,  0,  0,  0,  0,
+    80, 80, 80, 80, 80, 80, 80, 80,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    30, 30, 30, 30, 30, 30, 30, 30,
+    20, 20, 20, 20, 20, 20, 20, 20,
+    10, 10, 10, 10, 10, 10, 10, 10,
+    10, 10, 10, 10, 10, 10, 10, 10,
+    0,  0,  0,  0,  0,  0,  0,  0
+};
+
+const int KING_EG_TABLE[64] = {
+    -50,-40,-30,-20,-20,-30,-40,-50,
+    -30,-20,-10,  0,  0,-10,-20,-30,
+    -30,-10, 20, 30, 30, 20,-10,-30,
+    -30,-10, 30, 40, 40, 30,-10,-30,
+    -30,-10, 30, 40, 40, 30,-10,-30,
+    -30,-10, 20, 30, 30, 20,-10,-30,
+    -30,-30,  0,  0,  0,  0,-30,-30,
+    -50,-30,-30,-30,-30,-30,-30,-50
+};
+
+const int* mgTablePointer[] = {PAWN_MG_TABLE, KNIGHT_TABLE, BISHOP_TABLE, ROOK_TABLE, QUEEN_TABLE, KING_MG_TABLE};
+const int* egTablePointer[] = {PAWN_EG_TABLE, KNIGHT_TABLE, BISHOP_TABLE, ROOK_TABLE, QUEEN_TABLE, KING_EG_TABLE};
+
 const int PIECE_TO_SCORE[5] = {100, 300, 320, 500, 900};
 
-int evaluateLegalPos(Position* pos) {
-    // from perspective of stm
-    int white_score = 0;
-    for (int p_type = WHITE_PAWN; p_type <= WHITE_QUEEN; p_type++) {
-        white_score += PIECE_TO_SCORE[p_type % 6] * __builtin_popcountll(pos->pieces[p_type]);
-    }
-    for (int p_type = BLACK_PAWN; p_type <= BLACK_QUEEN; p_type++) {
-        white_score -= PIECE_TO_SCORE[p_type % 6] * __builtin_popcountll(pos->pieces[p_type]);
+// https://chessprogramming.org/Tapered_Eval
+int gamePhase(Position* pos) {
+    int phase = 0;
+    phase += __builtin_popcountll(pos->pieces[WHITE_KNIGHT] | pos->pieces[BLACK_KNIGHT]) * 1;
+    phase += __builtin_popcountll(pos->pieces[WHITE_BISHOP] | pos->pieces[BLACK_BISHOP]) * 1;
+    phase += __builtin_popcountll(pos->pieces[WHITE_ROOK] | pos->pieces[BLACK_ROOK]) * 2;
+    phase += __builtin_popcountll(pos->pieces[WHITE_QUEEN] | pos->pieces[BLACK_QUEEN]) * 4;
+
+    if (phase > 24) {
+        phase = 24;
     }
 
-    return (pos->stm == WHITE) ? white_score : -white_score;
+    return phase;
+}
+
+int evaluateLegalPos(Position* pos) {
+    int phase = gamePhase(pos);
+    int score = 0;
+
+    for (int sq = 0; sq < 64; sq++) {
+        int ptype = pos->squares[sq];
+        if (ptype == PIECE_NONE) {
+            continue;
+        }
+        int pieceIndex = ptype % 6;
+        int pieceColor = ptype / 6;
+        int flipped = (pos->stm == WHITE) ? sq ^ 56 : sq;
+
+        int mgScore = mgTablePointer[pieceIndex][flipped];
+        int egScore = egTablePointer[pieceIndex][flipped];
+        int psqtScore = ((mgScore * phase) + (24 - phase) * egScore) / 24;
+
+        int pieceValue = PIECE_TO_SCORE[pieceIndex] + psqtScore;
+
+        if (pieceColor == WHITE) {
+            score += pieceValue;
+        } else {
+            score -= pieceValue;
+        }
+    }
+
+    return (pos->stm == WHITE) ? score : -score;
 }
